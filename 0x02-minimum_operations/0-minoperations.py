e095ced0b4476847b14eca38b2da5cb67c6a8360
Radouane Baba <radoine.baba04@gmail.com>
2024-04-26 01:58:24 +0100

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,15 +4,14 @@
 
 def minOperations(n):
     """ Minimum number of operations """
-    prime_factors = [2, 3, 5, 7, 11, 13, 17, 19]
     if n <= 0:
         return 0
-    idx = 0
+    factor = 2
     steps = 0
     while (n != 1):
-        while (n % prime_factors[idx] != 0):
-            idx += 1
-        n /= prime_factors[idx]
-        steps += prime_factors[idx]
+        while (n % factor != 0):
+            factor += 1
+        n /= factor
+        steps += factor
 
     return steps
