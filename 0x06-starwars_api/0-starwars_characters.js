324689b2d171472c2068b31d5a8a037fddffeac0
Radouane Baba <radoine.baba04@gmail.com>
2024-05-23 19:02:33 +0100

    fix promise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,17 +2,27 @@
 const request = require('request');
 const { argv } = require('process');
 
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}/`;
 
-let url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}/`;
-
-request(url, (error, response, body) => {
-    if (error) console.log(error);
-    result = JSON.parse(body);
-    for (char_url of result['characters']) {
-        request(char_url, (err, resp, people) => {
-            if (err) console.log(err);
-            character = JSON.parse(people)
-            console.log(character.name);
+function makeRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
     });
-    }
-});
+  });
+}
+
+async function printCharacters () {
+  const characters = await makeRequest(url);
+  for (const charUrl of JSON.parse(characters).characters) {
+    const people = await makeRequest(charUrl);
+    const character = JSON.parse(people);
+    console.log(character.name);
+  }
+}
+
+printCharacters();
